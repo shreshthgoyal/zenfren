e7804c7d7031a9fcf00dc9b74f34b22f830bf566
Rishabh <rishabhkr.073@gmail.com>
2024-10-01 22:37:57 +0530

    moving service account credentials to env

--- pages/api/createDoc.js
+++ pages/api/createDoc.js
@@ -1,5 +1,4 @@
 import { google } from 'googleapis';
-import serviceAccount from '../../service-account-key.json';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
@@ -15,7 +14,18 @@ export default async function handler(req, res) {
   try {
     // Authenticate with the Google API
     const auth = new google.auth.GoogleAuth({
-      credentials: serviceAccount,
+      credentials: {
+        type: 'service_account',
+        project_id: process.env.GOOGLE_PROJECT_ID,
+        private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), 
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        auth_uri: process.env.GOOGLE_AUTH_URI,
+        token_uri: process.env.GOOGLE_TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
+        client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
+      },
       scopes: ['https://www.googleapis.com/auth/drive'],
     });
 
