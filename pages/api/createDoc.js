de64af9754b9b9d856f6390027406bd26e57d09b
shreshthgoyal <shreshthg30@gmail.com>
2024-10-02 01:19:05 +0530

    fix deploy

--- pages/api/createDoc.js
+++ pages/api/createDoc.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
         type: 'service_account',
         project_id: process.env.GOOGLE_PROJECT_ID,
         private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), 
+        private_key: process.env.GOOGLE_PRIVATE_KEY, 
         client_email: process.env.GOOGLE_CLIENT_EMAIL,
         client_id: process.env.GOOGLE_CLIENT_ID,
         auth_uri: process.env.GOOGLE_AUTH_URI,
